18167903384bc4d5c37dcf7726fe290b07963d0b
Mario Espinoza Aguayo <mario@magrocode.com>
2021-06-17 16:30:55 -0400

    first commit

---
+++ src/test/java/com/codelious/microservicios/app/zuul/microservicioszuul2/MicroserviciosZuul2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelious.microservicios.app.zuul.microservicioszuul2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosZuul2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
